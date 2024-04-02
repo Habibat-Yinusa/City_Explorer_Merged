6b8412ce74bec3210364d334411a63111e07ecf7
Mubarak <mubee2004@gmail.com>
2024-04-02 15:35:43 +0100

    initialized git repo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+
+function App() {
+  return (
+    <Box>
+      <Box>Hello</Box>
+    </Box>
+  );
+}
+
+export default App;
---
+++ src/store/user-slice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = JSON.parse(localStorage.getItem("userState")) || {
+  isAuth: false,
+  user: null,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.isAuth = true;
+      state.user = action.payload;
+      console.log(action.payload);
+      localStorage.setItem("userState", JSON.stringify(state));
+    },
+    logout: (state) => {
+      state.isAuth = false;
+      state.user = null;
+      localStorage.removeItem("userState");
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { login, logout } = userSlice.actions;
+
+export default userSlice.reducer;
