66c57e3262dc998d7e09298a2b2ee114fd9e285d
dev <yunusahabibat8@gmail.com>
2024-06-13 23:17:21 +0100

    get all

--- src/routes/bussinesRoute.ts
+++ src/routes/bussinesRoute.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getBusinessDetails, registerBusiness } from '../controllers/businessControllers';
+import { getAllBusinesses, getBusinessDetails, registerBusiness } from '../controllers/businessControllers';
 
 const router = Router();
 
 router.post('/register', registerBusiness);
 router.get('/:id', getBusinessDetails);
+router.get('/', getAllBusinesses);
 
 
 export default router;
