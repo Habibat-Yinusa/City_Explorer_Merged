06b3394331bd25b5a65028f052f8c5cf31f95357
dev <yunusahabibat8@gmail.com>
2024-06-14 23:45:11 +0100

    event controller

--- src/routes/bussinesRoute.ts
+++ src/routes/bussinesRoute.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { getAllBusinesses, getBusinessDetails, registerBusiness } from '../controllers/businessControllers';
+import { addEventToBusiness, getAllBusinesses, getBusinessDetails, registerBusiness } from '../controllers/businessControllers';
 
 const router = Router();
 
 router.post('/register', registerBusiness);
 router.get('/:id', getBusinessDetails);
 router.get('/', getAllBusinesses);
+router.post('/event/create/:id', addEventToBusiness)
 
 
 export default router;
