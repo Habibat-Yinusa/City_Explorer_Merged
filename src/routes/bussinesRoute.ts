241e0773c82248166472e6848a2cf153c849f9b1
dev <yunusahabibat8@gmail.com>
2024-06-15 23:06:02 +0100

    get all events

--- src/routes/bussinesRoute.ts
+++ src/routes/bussinesRoute.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { addEventToBusiness, getAllBusinesses, getAllEvents, getBusinessDetails, registerBusiness } from '../controllers/businessControllers';
+import { addEventToBusiness, getAllBusinesses, getEvents, getAllEvents, getBusinessDetails, registerBusiness } from '../controllers/businessControllers';
 
 const router = Router();
 
@@ -7,7 +7,8 @@ router.post('/register', registerBusiness);
 router.get('/:id', getBusinessDetails);
 router.get('/', getAllBusinesses);
 router.post('/createevent/:id', addEventToBusiness);
-router.get('/event/:id', getAllEvents)
+router.get('/event/:id', getEvents)
+router.get('/events', getAllEvents)
 
 
 export default router;
