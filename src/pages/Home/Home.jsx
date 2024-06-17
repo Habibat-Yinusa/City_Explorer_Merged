d88030912e351ab85f428d461adb3c5968ad572a
Mubarak <mubee2004@gmail.com>
2024-06-17 10:33:54 +0100

    deploying to pipeops

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,10 +4,23 @@ import { CenteredBox } from "../../styles/styled-components/styledBox";
 import burgerBanner from "../../assets/burgerbanner.svg";
 import img1 from "../../assets/img1.svg";
 import img2 from "../../assets/img2.svg";
-import { KeyboardArrowRight } from "@mui/icons-material";
+import {
+  BuildOutlined,
+  DryCleaningOutlined,
+  KeyboardArrowRight,
+  LibraryBooksOutlined,
+  LocalDrink,
+  LocalPizza,
+  ShoppingCart,
+  StoreOutlined,
+  ViewWeekRounded,
+} from "@mui/icons-material";
 import "@splidejs/react-splide/css";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <Box sx={{ backgroundColor: "#ececec" }}>
       <CenteredBox sx={{ backgroundColor: "#758BFD", borderRadius: "30px" }}>
@@ -38,15 +51,80 @@ const Home = () => {
           </Box>
         </CenteredBox>
       </CenteredBox>
+
+      <CenteredBox sx={{ marginTop: "1.2em", flexDirection: "column" }}>
+        <Typography
+          variant="h3"
+          sx={{
+            fontWeight: 700,
+            fontSize: { xs: "1.7rem", md: "2rem" },
+            width: "100%",
+          }}
+        >
+          Categories
+        </Typography>
+        <Grid container sx={{ margin: "2em 0" }}>
+          {categoryCard.map((item) => (
+            <Grid
+              item
+              xs="auto"
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+              }}
+              key={item.id}
+            >
+              <Box
+                sx={{
+                  maxWidth: "6em",
+                  width: "6em",
+                  cursor: "pointer",
+                  margin: ".4em 1em",
+                }}
+                onClick={() => navigate(item.link)}
+              >
+                <Box
+                  sx={{
+                    backgroundColor: "#fff",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    padding: "1em",
+                    borderRadius: "15px",
+                    // fontSize: "2rem",
+                    color: "#758BFD",
+                  }}
+                >
+                  {/* <BuildOutlined sx={{ fontSize: "2rem", color: "#758BFD" }} /> */}
+                  {item.icon}
+                </Box>
+                <Typography
+                  variant="body2"
+                  sx={{
+                    fontSize: "1rem",
+                    textTransform: "capitalize",
+                    textAlign: "center",
+                    marginTop: ".3em",
+                  }}
+                >
+                  {item.text}
+                </Typography>
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
+      </CenteredBox>
+
       <Box sx={{ flexDirection: "column" }}>
         <CenteredBox
           sx={{
             justifyContent: "space-between",
-            marginTop: "2em",
-            marginBottom: "2em",
+            marginTop: "1.2em",
+            marginBottom: "1.2em",
           }}
         >
-          hello
           <Typography
             variant="h3"
             sx={{ fontWeight: 700, fontSize: { xs: "1.7rem", md: "2rem" } }}
@@ -61,6 +139,7 @@ const Home = () => {
             }}
           />
         </CenteredBox>
+
         <Grid container spacing={2}>
           {businesses.map((item) => (
             <Grid item xs={3.1} sm={2.5} md={2.6} lg={2.3} key={item.id}>
@@ -82,6 +161,7 @@ const Home = () => {
             </Grid>
           ))}
         </Grid>
+
         <Grid
           container
           spacing={2}
@@ -186,6 +266,57 @@ const interests = [
     id: 5,
     text: "Capital Block is having an event this weekend that might interest you.",
     image: img2,
+  },
+];
+
+const categoryCard = [
+  {
+    id: 1,
+    icon: <BuildOutlined />,
+    text: "Auto repairs",
+    link: "#",
+  },
+  {
+    id: 2,
+    icon: <LocalPizza />,
+    text: "Takeout",
+    link: "#",
+  },
+  {
+    id: 3,
+    icon: <LocalDrink />,
+    text: "Smoothies",
+    link: "#",
+  },
+  {
+    id: 4,
+    icon: <StoreOutlined />,
+    text: "Saloon",
+    link: "#",
+  },
+  {
+    id: 5,
+    icon: <ShoppingCart />,
+    text: "Shopping",
+    link: "#",
+  },
+  {
+    id: 6,
+    icon: <LibraryBooksOutlined />,
+    text: "Book Stores",
+    link: "#",
+  },
+  {
+    id: 7,
+    icon: <DryCleaningOutlined />,
+    text: "Laundry",
+    link: "#",
+  },
+  {
+    id: 8,
+    icon: <ViewWeekRounded />,
+    text: "More",
+    link: "#",
   },
 ];
 
